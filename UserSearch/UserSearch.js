4522f10147a256fa697dd46c0fff10626b624c8c
Zak Burke <zburke@cornell.edu>
2019-02-13 08:39:30 -0500

    don't use the title attribute for html elements
    The title attribute should not be use for accessibility.
    
    Refs STCOM-296

--- UserSearch/UserSearch.js
+++ UserSearch/UserSearch.js
@@ -47,7 +47,6 @@ export default class UserSearch extends React.Component {
           key="searchButton"
           buttonStyle={this.props.searchButtonStyle}
           onClick={this.openModal}
-          title="Find User"
           tabIndex="-1"
         >
           {this.props.searchLabel ? this.props.searchLabel : <Icon icon="search" color="#fff" />}
