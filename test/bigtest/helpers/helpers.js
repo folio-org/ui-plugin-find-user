39ed7fa38d30b83216a2ebd6820e017b9acff098
John Coburn <jcoburn@ebsco.com>
2019-04-05 14:15:27 -0500

    add bigtest setup

---
+++ test/bigtest/helpers/helpers.js
@@ -0,0 +1,45 @@
+import { beforeEach } from '@bigtest/mocha';
+
+import setupStripesCore from '@folio/stripes-core/test/bigtest/helpers/setup-application';
+import {
+  withModules,
+  clearModules,
+} from '@folio/stripes-core/test/bigtest/helpers/stripes-config';
+import mirageOptions from '../network';
+
+export default function setupApplication({
+  scenarios,
+  hasAllPerms = true,
+} = {}) {
+  setupStripesCore({
+    mirageOptions,
+    scenarios,
+    stripesConfig: { hasAllPerms },
+
+    // setup a dummy app for smart components
+    modules: [{
+      type: 'app',
+      name: '@folio/ui-dummy',
+      displayName: 'dummy.title',
+      route: '/dummy',
+      module: null
+    }],
+
+    translations: {
+      'dummy.title': 'Dummy'
+    },
+  });
+}
+
+// replace the dummy app to mount the component
+export function mount(component) {
+  clearModules();
+
+  withModules([{
+    type: 'app',
+    name: '@folio/ui-dummy',
+    displayName: 'dummy.title',
+    route: '/dummy',
+    module: () => component
+  }]);
+}
---
+++ test/bigtest/tests/findUser-test.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { describe, beforeEach, it } from '@bigtest/mocha';
+import { expect } from 'chai';
+
+import setupApplication, { mount } from '../helpers/helpers';
+import PluginHarness from '../helpers/PluginHarness';
+import FindUserInteractor from '../interactors/findUser';
+let closeHandled = false;
+let userChosen = false;
+
+describe('UI-plugin-find-user', function () {
+  const findUser = new FindUserInteractor();
+  setupApplication();
+  
+
+  describe('rendering', function () {
+    
+    beforeEach(async function () {
+      await this.server.createList('user', 40);
+      this.visit('/dummy');
+      userChosen = false;
+      closeHandled = false;
+      mount(
+        <PluginHarness
+          selectUser={() => { userChosen = true; }}
+          afterClose={() => { closeHandled = true; }}
+        />
+      );
+    });
+
+    it('renders button', function () {
+      expect(
+        findUser.button.isPresent
+      ).to.be.true;
+    });
+
+    describe('click the button', function () {
+      beforeEach(async function () {
+        await findUser.button.click();
+      });
+
+      it('opens a modal', function () {
+        expect(
+          findUser.modal.isPresent
+        ).to.be.true;
+      });
+
+      it('focuses the search field', function () {
+        expect(
+          findUser.modal.searchField.isFocused
+        ).to.be.true;
+      });
+
+      describe('checking show inactive filter', function () {
+        beforeEach(async function () {
+          await findUser.modal.clickInactiveUsersCheckbox();
+          await findUser.modal.searchField.fill('t');
+        });
+
+        it('pulls a result set', function () {
+          expect(findUser.modal.instances().length).to.be.greaterThan(0);
+        });
+
+        describe('resetting the filter and search', function () {
+          beforeEach(async function () {
+            await findUser.modal.resetButton.click();
+          });
+
+          it('displays "No Results" message', function () {
+            expect(findUser.modal.noResultsDisplayed).to.be.true;
+          });
+
+          it('unchecks the filter checkboxes', function () {
+            expect(findUser.modal.filterCheckboxes().filter((cb) => cb.isChecked).length).to.equal(0);
+          });
+
+          it('clears the value in the search field', function () {
+            expect(findUser.modal.searchField.value).to.equal('');
+          });
+        });
+      });
+
+      describe('filling in the searchField', function () {
+        beforeEach(async function () {
+          await findUser.modal.searchField.fill('t');
+        });
+
+        it('activates the reset button', function () {
+          expect(findUser.modal.resetButton.isEnabled).to.be.true;
+        });
+
+        it('activates the search button', function () {
+          expect(findUser.modal.searchButton.isEnabled).to.be.true;
+        });
+
+        describe('submitting the search', function () {
+          beforeEach(async function () {
+            await findUser.modal.searchButton.click();
+          });
+
+          it('returns a set of results', function () {
+            expect(findUser.modal.instances().length).to.be.greaterThan(0);
+          });
+
+          describe('selecting a user', function () {
+            beforeEach(async function () {
+              await findUser.modal.instances(1).click();
+            });
+
+            it('hides the modal', function () {
+              expect(closeHandled).to.be.true;
+            });
+
+            it('calls the selectUser callback', function () {
+              expect(userChosen).to.be.true;
+            });
+          });
+        });
+      });
+    });
+  });
+});
