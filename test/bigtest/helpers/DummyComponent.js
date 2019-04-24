f903b79f9a4dcb6d570cd02e464770ea7151abab
John Coburn <jcoburn@ebsco.com>
2019-04-24 15:01:17 -0500

    add console output for dummy app

--- test/bigtest/helpers/DummyComponent.js
+++ test/bigtest/helpers/DummyComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class DummyApp extends Component {
   render() {
+    console.log('rendering Dummy App');
     return (<h1>Future of libraries is open!!</h1>);
   }
 }
