980402e44e4bfc3a473289ac58ce60fbf328e5f3
Mikita Siadykh <mikita_siadykh@epam.com>
2024-10-25 12:02:34 +0300

    UIPFU-100 Add modal title prop support (#280)

--- src/UserSearchModal.js
+++ src/UserSearchModal.js
@@ -23,6 +23,7 @@ class UserSearchModal extends Component {
     dataKey: PropTypes.string,
     contentRef: PropTypes.object,
     modalRef: PropTypes.object,
+    modalTitle: PropTypes.node,
     restoreFocus: PropTypes.bool,
     initialSelectedUsers: PropTypes.shape({
       [PropTypes.string]: PropTypes.shape({
@@ -91,6 +92,7 @@ class UserSearchModal extends Component {
       initialSelectedUsers,
       tenantId,
       stripes,
+      modalTitle,
     } = this.props;
 
     return (
@@ -98,7 +100,7 @@ class UserSearchModal extends Component {
         contentClass={css.modalContent}
         dismissible
         enforceFocus={false}
-        label={<FormattedMessage id="ui-plugin-find-user.modal.label" />}
+        label={modalTitle || <FormattedMessage id="ui-plugin-find-user.modal.label" />}
         open={openWhen}
         ref={this.modalRef}
         size="large"
