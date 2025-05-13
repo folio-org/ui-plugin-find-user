047b5fcd1ab12661a05ad4d72156e4c9a40c09e6
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2025-05-13 10:11:18 +0300

    Remove no longer used bigtests

--- test/bigtest/interactors/application.js
+++
@@ -1,9 +0,0 @@
-import { interactor, isPresent } from '@bigtest/interactor';
-
-// https://bigtestjs.io/guides/interactors/introduction/
-@interactor class ApplicationInteractor {
-  static defaultScope = '#ModuleContainer';
-  pluginNotFound = isPresent('[data-test-no-plugin-available]');
-}
-
-export default ApplicationInteractor;
